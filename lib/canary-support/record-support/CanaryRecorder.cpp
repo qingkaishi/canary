e97da88d127ab97975566961485e691b2f60bef2
qingkaishi <qingkaishi@gmail.com>
2014-04-21 14:55:40 +0800

    ...

--- lib/canary-support/record-support/CanaryRecorder.cpp
+++ lib/canary-support/record-support/CanaryRecorder.cpp
@@ -8,6 +8,8 @@
 #include <sys/time.h>
 #include "Signature.h"
 #include "SignalRoutine.h"
+#include "util/cvector.h"
+#include <boost/unordered_map.hpp>
 
 #define POSIX_MUTEX
 
@@ -16,13 +18,30 @@
 #define MAX_LOG_LEN 50000
 #define MAX_THREAD_NUM 50
 
+typedef struct {
+    cvector VAL_LOG; // <void *>
+    cvector VER_LOG; // <unsigned>
+} RLOG;
+
+typedef struct {
+    cvector VER_LOG; // <unsigned>
+} WLOG;
+
+cvector W_VERSION;
+
+typedef struct {
+    cvector SYN_LOG; // <unsigned> record ti
+} LLOG;
+
+static LLOG llog;
+
+static pthread_key_t rlog_key;
+static pthread_key_t wlog_key;
+
+static boost::unordered_map<pthread_t, unsigned> thread_ht;
+static boost::unordered_map<void *, unsigned> mutex_ht;
+
 static bool start = false;
-static pthread_t thread_idx_map[MAX_THREAD_NUM];
-static int thread_idx = 1; //start from 1, make 0 be a terminal
-
-static unsigned **GLOG = NULL;
-static unsigned *GIDX = NULL;
-
 static int num_shared_vars = 0;
 
 // set when log is too long
@@ -30,73 +49,75 @@ static bool stop_recording = false;
 
 //static struct timeval tpstart, tpend;
 
-int static inline threadid(pthread_t tid) {
-    for (int i = 1; i < thread_idx; i++) {
-        if (thread_idx_map[i] == tid) {
-            return i;
-        }
-    }
+/* delete logs
+ */
+void close_read_log(void* log) {
+    /// @TODO dump
 
-    return -1;
+    for (unsigned i = 0; i < num_shared_vars; i++) {
+        cvector_destroy(((RLOG*) log)[i]->VAL_LOG);
+        cvector_destroy(((RLOG*) log)[i]->VER_LOG);
+    }
+    delete [] (RLOG*) log;
 }
 
-void static inline threadcreate(pthread_t tid) {
-    thread_idx_map[thread_idx++] = tid;
-    
-    if (thread_idx > MAX_THREAD_NUM) {
-        printf("Too many threads!\n");
-        exit(0);
+void close_write_log(void* log) {
+    /// @TODO dump
+
+    for (unsigned i = 0; i < num_shared_vars; i++) {
+        cvector_destroy(((WLOG*) log)[i]->VER_LOG);
     }
+    delete [] (WLOG*) log;
 }
 
-void static inline store(int svId, int tid) {
-    if(stop_recording){
+void static inline storeRead(unsigned version, void* value, RLOG* rlog) {
+}
+
+void static inline storeWrite(unsigned version, WLOG* wlog) {
+    if (stop_recording) {
         return;
     }
 
-    int currentIdx = GIDX[svId];
-    if(currentIdx >= MAX_LOG_LEN){
-	printf("Log is too long to record! Stop recording!\n");
-	stop_recording = true;
-	return;
+    int log_len = cvector_length(wlog->VER_LOG);
+    if (log_len >= MAX_LOG_LEN) {
+        printf("Log is too long to record! Stop recording!\n");
+        stop_recording = true;
+        return;
     }
 
-    if (currentIdx > 0 && (int) (GLOG[svId][currentIdx - 2]) == tid) {
-        GLOG[svId][currentIdx - 1]++;
+    unsigned x = (unsigned) cvector_at(wlog->VER_LOG, log_len - 1);
+    unsigned y = (unsigned) cvector_at(wlog->VER_LOG, log_len - 2);
+    if (log_len > 0 && x + y == version) {
+        cvector_insert_at(wlog->VER_LOG, log_len - 1, (void *) (x + 1));
     } else {
-        GLOG[svId][currentIdx] = tid;
-        GLOG[svId][currentIdx + 1] = 1;
-        GIDX[svId] += 2;
+        cvector_insert_at(wlog->VER_LOG, log_len, (void *) (version));
+        cvector_insert_at(wlog->VER_LOG, log_len + 1, (void *) (1));
     }
+}
+
+void static inline storeSync(unsigned syncId, unsigned tId){
 }
 
 extern "C" {
 
     void OnInit(int svsNum) {
         printf("OnInit-Record\n");
+        num_shared_vars = svsNum;
         initializeSigRoutine();
 
-        //start = true;
-        num_shared_vars = svsNum + 2; // one for fork, and the other is for synchronizations
+        pthread_key_create(&rlog_key, close_read_log);
+        pthread_key_create(&wlog_key, close_write_log);
 
-        for (int i = 0; i < MAX_THREAD_NUM; i++) {
-            thread_idx_map[i] = 0;
-        }
-
-        initialize(num_shared_vars); // initialize locks
-
-        GLOG = new unsigned*[num_shared_vars];
-        GIDX = new unsigned[num_shared_vars];
-        for (int i = 0; i < num_shared_vars; i++) {
-            GLOG[i] = new unsigned[MAX_LOG_LEN];
-            GIDX[i] = 0;
-        }
+        W_VERSION = cvector_create(sizeof (unsigned));
+        cvector_resize(W_VERSION, svsNum);
+        
+        llog.SYN_LOG = cvector_create(sizeof (unsigned));
+        
+        initialize(svsNum + 2); // initialize locks, add one for thread_ht, the other for mutex_ht
 
         // main thread.
         pthread_t tid = pthread_self();
-        if (threadid(tid) == -1) {
-            threadcreate(tid);
-        }
+        thread_ht[tid] = thread_ht.size();
 
         //gettimeofday(&tpstart, NULL);
     }
@@ -108,85 +129,47 @@ extern "C" {
         //double timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) + tpend.tv_usec - tpstart.tv_usec;
         //timeuse /= 1000;
         //printf("processor time is %lf ms\n", timeuse);
-
-        Sig* sig = new Sig;
-        strcpy(sig->recorder, "leap");
-
-        FILE * fout = fopen("log.replay.dat", "wb");
-        printf("OnExit-Record\n");
-
-        fwrite(sig, sizeof (Sig), 1, fout);
-        fwrite(GIDX, sizeof (unsigned), num_shared_vars, fout);
-        for (int i = 0; i < num_shared_vars; i++) {
-            fwrite(GLOG[i], sizeof (unsigned), GIDX[i], fout);
-        }
-        fclose(fout);
-
-#ifdef DEBUG
-        // debug out
-        FILE* fdebug = fopen("log.debug", "w+");
-        for (int i = 0; i < num_shared_vars; i++) {
-            fprintf(fdebug, "%d, ", GIDX[i]);
-        }
-        fprintf(fdebug, "\n\n");
-        for (int i = 0; i < num_shared_vars; i++) {
-            for (unsigned j = 0; j < GIDX[i]; j++) {
-                fprintf(fdebug, "%d, ", GLOG[i][j]);
-            }
-            fprintf(fdebug, "\n");
-        }
-        fclose(fdebug);
-#endif
+        
+        ///@TODO dump llog
     }
 
-    void OnPreLoad(int svId, int debug) {
+    void OnLoad(int svId, void* value, int debug) {
         if (!start) {
             return;
         }
 
-        pthread_t tid = pthread_self();
-        int _tid = -1;
-        do {
-            _tid = threadid(tid);
-        } while (_tid < 0);
-
-        lock(svId);
-
-#ifdef DEBUG
-        printf("OnPreLoad: %d at t%d [%d]\n", svId, _tid, debug);
-#endif
-        store(svId, _tid);
-    }
-
-    void OnLoad(int svId, int debug) {
-        if (!start) {
-            return;
-        }
 #ifdef DEBUG
         printf("OnLoad\n");
 #endif
-        unlock(svId);
+        // using thread_key to tell whether log has been established
+        // if so, use it, otherwise, new one
+        RLOG * rlog = (RLOG*) pthread_getspecific(rlog_key);
+        if (rlog == NULL) {
+            rlog = new RLOG[num_shared_vars];
+            pthread_setspecific(rlog_key, rlog);
+        }
+        if (rlog[svId] == NULL)
+            rlog[svId]->VAL_LOG = cvector_create(sizeof (void*));
+        rlog[svId]->VER_LOG = cvector_create(sizeof (unsigned));
+        storeRead((unsigned) cvector_at(W_VERSION, svId), value, rlog[svId]); /// @FIXME version, value, rlog[svid]
     }
 
-    void OnPreStore(int svId, int debug) {
+    unsigned OnPreStore(int svId, int debug) {
         if (!start) {
             return;
         }
 
-        pthread_t tid = pthread_self();
-        int _tid = -1;
-        do {
-            _tid = threadid(tid);
-        } while (_tid < 0);
-
         lock(svId);
-#ifdef DEBUG        
+#ifdef DEBUG
         printf("OnPreStore: %d at t%d [%d]\n", svId, _tid, debug);
 #endif
-        store(svId, _tid);
+        unsigned version = W_VERSION[svId];
+        W_VERSION[svId]++;
+
+        return version;
     }
 
-    void OnStore(int svId, int debug) {
+    void OnStore(int svId, unsigned version, int debug) {
         if (!start) {
             return;
         }
@@ -194,20 +177,15 @@ extern "C" {
         printf("OnStore\n");
 #endif
         unlock(svId);
-    }
 
-    void OnPreLock(int nouse) {
-#ifdef DEBUG
-        if (!start) {
-            return;
+        WLOG * wlog = (WLOG*) pthread_getspecific(wlog_key);
+        if (wlog == NULL) {
+            wlog = new WLOG[num_shared_vars];
         }
-        pthread_t tid = pthread_self();
-        int _tid = -1;
-        do {
-            _tid = threadid(tid);
-        } while (_tid < 0);
-        printf("OnPreLock[%d]\n", _tid);
-#endif
+        if (wlog[svId] == NULL) {
+            wlog[svId]->VER_LOG = cvector_create(sizeof (unsigned));
+        }
+        storeWrite(version, wlog[svId]); // version, wlog
     }
 
     void OnLock(int nouse) {
@@ -226,34 +204,18 @@ extern "C" {
 #endif
 
     }
-
-    void OnPreUnlock(int nouse) {
-#ifdef DEBUG
-        if (!start) {
-            return;
-        }
-        printf("OnpreunLock\n");
-#endif
+    
+    void OnPreMutexInit(pthread_mutex_t* mptr, bool race){
+    
+    }
+    
+    void OnMutexInit(pthread_mutex_t* mptr, bool race){
+    
     }
 
-    void OnUnlock(int nouse) {
-#ifdef DEBUG
-        if (!start) {
-            return;
-        }
-        pthread_t tid = pthread_self();
-        int _tid = -1;
-        do {
-            _tid = threadid(tid);
-        } while (_tid < 0);
-        printf("OnunLock <-- t%d\n", _tid);
-#endif
-    }
-
-    void OnPreFork(int nouse) {
+    void OnPreFork(bool race) {
         if (!start) {
             start = true;
-            //return;
         }
 #ifdef DEBUG
         printf("OnPreFork\n");
@@ -261,7 +223,7 @@ extern "C" {
         forklock(num_shared_vars - 1);
     }
 
-    void OnFork(long forked_tid_ptr) {
+    void OnFork(long forked_tid_ptr, bool race) {
         if (!start) {
             return;
         }
@@ -282,28 +244,7 @@ extern "C" {
         forkunlock(num_shared_vars - 1);
     }
 
-    void OnPreJoin(int id) {
-    }
-
-    void OnJoin(int id) {
-    }
-
-    void OnPreWait(int condId) {
-        if (!start) {
-            return;
-        }
-#ifdef DEBUG        
-        printf("OnPrewait\n");
-#endif
-        pthread_t tid = pthread_self();
-        int _tid = -1;
-        do {
-            _tid = threadid(tid);
-        } while (_tid < 0);
-        store(num_shared_vars - 2, _tid);
-    }
-
-    void OnWait(int condId, long cond_ptr, long mutex_ptr) {
+    void OnWait(int condId, long cond_ptr, pthread_mutex_t* mutex_ptr) {
         if (!start) {
             return;
         }
@@ -316,32 +257,6 @@ extern "C" {
             _tid = threadid(tid);
         } while (_tid < 0);
         store(num_shared_vars - 2, _tid);
-    }
-
-    void OnPreNotify(int condId) {
-        if (!start) {
-            return;
-        }
-#ifdef DEBUG
-        printf("OnPreNotify\n");
-#endif
-        lock(num_shared_vars - 2);
-    }
-
-    void OnNotify(int condId) {
-        if (!start) {
-            return;
-        }
-#ifdef DEBUG
-        printf("OnNotify\n");
-#endif
-        pthread_t tid = pthread_self();
-        int _tid = -1;
-        do {
-            _tid = threadid(tid);
-        } while (_tid < 0);
-        store(num_shared_vars - 2, _tid);
-        unlock(num_shared_vars - 2);
     }
 }
 
