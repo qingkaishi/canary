0331e22a5ec1c852fdeddf8a23f0365c6498b4d5
qingkaishi <qingkaishi@gmail.com>
2014-06-02 11:24:22 +0800

    init canary replayer support

--- lib/canary-support/replay-support/CanaryReplayer.cpp
+++ lib/canary-support/replay-support/CanaryReplayer.cpp
@@ -7,105 +7,51 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-#include "SignalRoutine.h"
-
-#ifdef NO_TIME_CMD
-static struct timeval tpstart, tpend;
-#endif
-
-bool start = false;
-
 extern "C" {
 
     void OnInit(unsigned svsNum) {
-        printf("OnInit-Record\n");
-
-#ifdef NO_TIME_CMD
-        gettimeofday(&tpstart, NULL);
-#endif
+        printf("OnInit-Record (canary replayer)\n");
     }
 
     void OnExit() {
-        start = false;
-        
-#ifdef NO_TIME_CMD
-        gettimeofday(&tpend, NULL);
-        double timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) + tpend.tv_usec - tpstart.tv_usec;
-        timeuse /= 1000;
-        printf("processor time is %lf ms\n", timeuse);
-#endif
-        printf("OnExit-Record\n");
+        printf("OnExit-Record (canary replayer)\n");
     }
 
     void OnAddressInit(void* value, size_t size, size_t n) {
-        if (!start) {
-            return;
-        }
-
     }
 
     void OnLoad(int svId, long address, long value, int debug) {
-        if (!start) {
-            return;
-        }
     }
 
     unsigned OnPreStore(int svId, int debug) {
-        if (!start) {
-            return 0;
-        }
-
         return 0;
     }
 
     void OnStore(int svId, unsigned version, long address, long value, int debug) {
-        if (!start) {
-            return;
-        }
     }
 
     void OnLock(pthread_mutex_t* mutex_ptr) {
-        if (!start) {
-            return;
-        }
     }
 
     void OnWait(pthread_cond_t* cond_ptr, pthread_mutex_t* mutex_ptr) {
-        if (!start) {
-            return;
-        }
     }
 
     void OnPreMutexInit(bool race) {
-        if (start && race) {
-        }
     }
 
     void OnMutexInit(pthread_mutex_t* mutex_ptr, bool race) {
     }
 
     void OnPreFork(bool race) {
-        if (!start) {
-            start = true;
-        }
     }
 
     void OnFork(pthread_t* forked_tid_ptr, bool race) {
-        if (!start) {
-            return;
-        }
     }
 
+    void OnJoin(pthread_t tid, bool race) {
+    }
 
-}
+    void OnLocal(long value, int id) {
+    }
 
-/* ************************************************************************
- * Signal Process
- * ************************************************************************/
-
-void sigroutine(int dunno) {
-    printSigInformation(dunno);
-
-    OnExit();
-    exit(dunno);
 }
