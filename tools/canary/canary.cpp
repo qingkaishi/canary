52610e2a12c84bd57f1cd8e176e87e36f49416b7
Yang Lin <yanglin@smail.nju.edu.cn>
2024-04-05 14:01:14 +0800

    change the main analysis pass as memory leak detect pass

--- tools/canary/canary.cpp
+++ tools/canary/canary.cpp
@@ -30,10 +30,12 @@
 #include <llvm/Support/Signals.h>
 #include <llvm/Support/ToolOutputFile.h>
 #include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Scalar/SimplifyCFG.h>
 #include <llvm/Transforms/Utils.h>
 
 #include <memory>
 
+#include "MemoryLeak/MLDValueFlowAnalysis.h"
 #include "NullPointer/NullCheckAnalysis.h"
 #include "Support/RecursiveTimer.h"
 #include "Support/Statistics.h"
@@ -98,17 +100,17 @@ int main(int argc, char **argv) {
 
     auto *TransformTimer = new RecursiveTimerPass("Transforming the bitcode");
     Passes.add(TransformTimer->start());
-    Passes.add(createLowerAtomicPass());
-    Passes.add(createLowerInvokePass());
-    Passes.add(createPromoteMemoryToRegisterPass());
-    Passes.add(createSCCPPass());
-    Passes.add(createLoopSimplifyPass());
+    // Passes.add(createLowerAtomicPass());
+    // Passes.add(createLowerInvokePass());
+    // Passes.add(createPromoteMemoryToRegisterPass());
+    // Passes.add(createSCCPPass());
+    // Passes.add(createLoopSimplifyPass());
     Passes.add(new LowerConstantExpr());
     Passes.add(TransformTimer->done());
     if (!OutputAssembly.getValue()) {
         auto *AnalysisTimer = new RecursiveTimerPass("Analyzing the bitcode");
         Passes.add(AnalysisTimer->start());
-        Passes.add(new NullCheckAnalysis());
+        Passes.add(new MLDValueFlowAnalysis());
         Passes.add(AnalysisTimer->done());
     }
 
