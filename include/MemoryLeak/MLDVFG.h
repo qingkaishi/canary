878951c7a907a8b14730812aa7cf5cf5600e0cfb
Yang Lin <yanglin@smail.nju.edu.cn>
2024-04-09 18:31:42 +0800

    fix the algorithm in cfl reachability

--- include/MemoryLeak/MLDVFG.h
+++ include/MemoryLeak/MLDVFG.h
@@ -3,22 +3,28 @@
 #include "DyckAA/DyckCallGraph.h"
 #include "DyckAA/DyckGraphNode.h"
 #include "DyckAA/DyckVFG.h"
-#include <iterator>
 #include <llvm/IR/Value.h>
 #include <map>
 #include <set>
+#include <stack>
+#include <utility>
+#include <vector>
 
 class Slice {
 
-friend class MLDVFG;
+    friend class MLDVFG;
     DyckVFGNode *Source;
     std::set<DyckVFGNode *> ReachableSet;
     std::set<DyckVFGNode *> Sinks;
 
   public:
+    Slice()
+        : Source(nullptr) {}
     Slice(DyckVFGNode *Source)
-        : Source(Source){};
-    ~Slice(){};
+        : Source(Source) {}
+    // Slice(Slice &other):Source(other.Source), ReachableSet(other.ReachableSet), Sinks(other.Sinks){
+    // }
+    ~Slice() {}
 
     void addReachable(DyckVFGNode *Node) {
         ReachableSet.insert(Node);
@@ -26,17 +32,33 @@ friend class MLDVFG;
     void addSink(DyckVFGNode *Node) {
         Sinks.insert(Node);
     }
-    bool contains(DyckVFGNode *Node){
+    bool contains(DyckVFGNode *Node) {
         return ReachableSet.find(Node) != ReachableSet.end();
     }
-    bool sinkSetEmpty(){
+    bool contains(DyckVFGNode *Node) const {
+        return ReachableSet.find(Node) != ReachableSet.end();
+    }
+    bool sinkSetEmpty() {
         return Sinks.empty();
     }
-    std::set<DyckVFGNode *>::const_iterator sinksBegin(){
+    DyckVFGNode *getSource() const {
+        return Source;
+    }
+    DyckVFGNode *getSource() {
+        return Source;
+    }
+    std::set<DyckVFGNode *>::const_iterator sinks_begin() {
         return Sinks.begin();
     }
-    std::set<DyckVFGNode*>::const_iterator sinksEnd(){
+    std::set<DyckVFGNode *>::const_iterator sinks_end() {
         return Sinks.end();
+    }
+
+    std::set<DyckVFGNode *>::const_iterator reach_begin() {
+        return ReachableSet.begin();
+    }
+    std::set<DyckVFGNode *>::const_iterator reach_end() {
+        return ReachableSet.end();
     }
 };
 
@@ -45,17 +67,65 @@ class MLDVFG {
     DyckCallGraph *DyckCG;
     std::map<DyckVFGNode *, Slice> LeakMap;
     std::set<DyckVFGNode *> SourcesSet;
-    using Context = std::stack<int>;
-    // std::unordered_map<DyckVFGNode * , std::set<DyckVFGNode *>> ValSrcReachable;
-    // std::unordered_map<DyckVFGNode * , std::set<DyckVFGNode *>> ValSrcSinks;
     void ForwardReachable(DyckVFGNode *);
     void BackwardReachable(DyckVFGNode *);
-    void dyckReachable(DyckVFGNode *);
+    // void dyckForwardReachable(DyckVFGNode *);
+
+    struct Context {
+        std::vector<int> instance;
+        Context()
+            : instance() {}
+        Context(const Context &other)
+            : instance(other.instance) {}
+        int top() const {
+            return instance.back();
+        }
+        void push(int i) {
+            instance.push_back(i);
+        }
+        bool empty() const {
+            return instance.empty();
+        }
+        void pop() {
+            instance.pop_back();
+        }
+        bool count(int i) const {
+            for(auto j : instance){
+                if(i == j){
+                    return true;
+                }
+            }
+            return false;
+        }
+        bool const operator<(const Context &other) const {
+            bool less = true;
+            for (int i = 0; i < other.instance.size() && i < instance.size(); i++) {
+                if (less) {
+                    less = less && instance[i] < other.instance[i];
+                }
+                else {
+                    break;
+                }
+            }
+            return less;
+        }
+    };
+    using NodeContextTy = std::pair<DyckVFGNode *, Context>;
 
   public:
     MLDVFG(DyckVFG *VFG, DyckCallGraph *DyckCG);
+
+    std::map<DyckVFGNode *, Slice>::iterator begin() {
+        return LeakMap.begin();
+    }
+    std::map<DyckVFGNode *, Slice>::iterator end() {
+        return LeakMap.end();
+    }
     ~MLDVFG();
+
     void printVFG() const;
+    void printSlice(Slice &Slice) const;
+    void printSubGraph(std::set<DyckVFGNode *>, std::string) const;
 };
 
 #endif // MEMORYLEAK_MLDVFG_H
--- lib/MemoryLeak/MLDVFG.cpp
+++ lib/MemoryLeak/MLDVFG.cpp
@@ -1,20 +1,17 @@
 #include "MemoryLeak/MLDVFG.h"
 // #include "DyckAA/DyckGraphNode.h"
-#include "DyckAA/DyckGraphNode.h"
 #include "DyckAA/DyckVFG.h"
 #include "Support/API.h"
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
+#include <iostream>
 #include <queue>
 #include <set>
-#include <stack>
+#include <string>
 #include <system_error>
-#include <unordered_map>
 #include <utility>
-#include <vector>
-// #include <vector>
 
 MLDVFG::MLDVFG(DyckVFG *VFG, DyckCallGraph *DyckCG)
     : VFG(VFG),
@@ -23,49 +20,61 @@ MLDVFG::MLDVFG(DyckVFG *VFG, DyckCallGraph *DyckCG)
         if (isa<Instruction>((*VFGNodeIt)->getValue()) && API::isHeapAllocate((Instruction *)(*VFGNodeIt)->getValue())) {
             SourcesSet.insert(*VFGNodeIt);
             ForwardReachable(*VFGNodeIt);
-
-            // dyckReachable(*VFGNodeIt);
+            BackwardReachable(*VFGNodeIt);
+            // printSlice(LeakMap[*VFGNodeIt]);
         }
     }
 }
 
+MLDVFG::~MLDVFG() {
+    delete this;
+}
+
 void MLDVFG::ForwardReachable(DyckVFGNode *VFGNode) {
     Slice ForwardSlice(VFGNode);
-    std::stack<std::pair<DyckVFGNode *, Context>> DfsStack;
-    DfsStack.push({VFGNode, Context()});
-    while (!DfsStack.empty()) {
-        auto CurrPair = DfsStack.top();
-        DfsStack.pop();
-        if (ForwardSlice.contains(CurrPair.first)) {
-            continue;
-        }
+    std::map<DyckVFGNode *, std::set<Context>> ForwardCFLReach;
+    std::queue<NodeContextTy> Worklist;
+    Worklist.push({VFGNode, Context()});
+    int idx = 0;
+    while (!Worklist.empty()) {
+        auto CurrPair = Worklist.front();
+        Worklist.pop();
         /// add into reachable set
-        if (isa<Argument>(CurrPair.first) && API::isRustSinkForC(dyn_cast<Argument>(CurrPair.first))) {
+        if (isa<Argument>(CurrPair.first->getValue()) && API::isRustSinkForC(dyn_cast<Argument>(CurrPair.first->getValue()))) {
             ForwardSlice.addSink(CurrPair.first);
         }
         ForwardSlice.addReachable(CurrPair.first);
         for (auto Edge = CurrPair.first->begin(); Edge != CurrPair.first->end(); Edge++) {
-            if (ForwardSlice.contains(Edge->first))
-                continue;
             /// cfl match
             /// if label > 0, then it's a call edge, push into context stack
+            auto NewCxt(CurrPair.second);
             if (Edge->second > 0) {
-                auto NewCxt(CurrPair.second);
-                NewCxt.push(Edge->second);
-                DfsStack.push({Edge->first, NewCxt});
+                if (!NewCxt.count(Edge->second))
+                    NewCxt.push(Edge->second);
             }
             /// if label <0 , then it'a a ret edge, pop call info from stack
             else if (Edge->second < 0) {
-                if (!CurrPair.second.empty() && CurrPair.second.top() != -Edge->second)
-                    continue;
-                auto NewCxt(CurrPair.second);
-                NewCxt.pop();
-                DfsStack.push({Edge->first, NewCxt});
+                if (!NewCxt.empty()) {
+                    if (NewCxt.top() == -Edge->second) {
+                        NewCxt.pop();
+                    }
+                    else {
+                        continue;
+                    }
+                }
             }
-            // normal edge, just push
+            if (ForwardCFLReach.count(Edge->first)) {
+                auto It = ForwardCFLReach.find(Edge->first);
+                if (!It->second.count(NewCxt)) {
+                    ForwardCFLReach[Edge->first].emplace(NewCxt);
+                    auto NodeCxt = std::make_pair(Edge->first, NewCxt);
+                    Worklist.push(NodeCxt);
+                }
+            }
             else {
-                auto NewCxt(CurrPair.second);
-                DfsStack.push({Edge->first, NewCxt});
+                ForwardCFLReach[Edge->first].emplace(NewCxt);
+                auto NodeCxt = std::make_pair(Edge->first, NewCxt);
+                Worklist.push(NodeCxt);
             }
         }
     }
@@ -78,7 +87,7 @@ void MLDVFG::BackwardReachable(DyckVFGNode *VFGNode) {
     if (LeakMap[VFGNode].sinkSetEmpty()) {
         return;
     }
-    for (auto Sink = ForwardSlice.sinksBegin(); Sink != ForwardSlice.sinksEnd(); Sink++) {
+    for (auto Sink = ForwardSlice.sinks_begin(); Sink != ForwardSlice.sinks_end(); Sink++) {
         std::queue<DyckVFGNode *> worklist;
         BackwardSlice.addSink(*Sink);
         BackwardSlice.addReachable(*Sink);
@@ -97,62 +106,71 @@ void MLDVFG::BackwardReachable(DyckVFGNode *VFGNode) {
     ForwardSlice = std::move(BackwardSlice);
 }
 
-// void MLDVFG::printVFG() const {
-//     auto LabelGenerator = [](Value *Val) {
-//         std::string Label;
-//         raw_string_ostream LabselBuilder(Label);
-//         if (auto Inst = dyn_cast<Instruction>(Val)) {
+void MLDVFG::printVFG() const {
+    std::set<DyckVFGNode *> AllInOne;
+    for (auto ValReach : LeakMap) {
+        AllInOne.insert(ValReach.second.reach_begin(), ValReach.second.reach_end());
+    }
+    printSubGraph(AllInOne, "vfg.dot");
+}
 
-//             LabselBuilder << *Inst;
-//         }
-//         else if (auto Arg = dyn_cast<Argument>(Val)) {
-//             LabselBuilder << Arg->getParent()->getName() << "\n" << *Arg;
-//         }
-//         auto LabelIdx = Label.find('\"');
-//         while (LabelIdx != std::string::npos) {
-//             Label.insert(LabelIdx, "\\");
-//             LabelIdx = Label.find('\"', LabelIdx + 2);
-//         }
-//         return Label;
-//     };
-//     std::set<DyckVFGNode *> AllInOne;
-//     for (auto ValReach : ValSrcReachable) {
-//         AllInOne.insert(ValReach.second.begin(), ValReach.second.end());
-//     }
-//     AllInOne.insert(ValSrcSet.begin(), ValSrcSet.end());
+void MLDVFG::printSlice(Slice &S) const {
+    std::set<DyckVFGNode *> AllInOne;
+    AllInOne.insert(S.reach_begin(), S.reach_end());
+    printSubGraph(AllInOne, ("slice" + std::to_string((uint64_t)S.getSource()) + ".dot"));
+}
 
-//     std::map<DyckVFGNode *, int> VFGNode2Idx;
+void MLDVFG::printSubGraph(std::set<DyckVFGNode *> AllInOne, std::string Filename) const {
+    auto LabelGenerator = [](Value *Val) {
+        std::string Label;
+        raw_string_ostream LabselBuilder(Label);
+        if (auto Inst = dyn_cast<Instruction>(Val)) {
 
-//     int NodeIdx = 0;
-//     std::error_code EC;
-//     raw_fd_stream VFGDot("vfg.dot", EC);
-//     VFGDot << "digraph rel{shape=box\n";
-//     for (auto VFGNode : AllInOne) {
-//         Value *Val = VFGNode->getValue();
-//         std::string Color("black");
-//         if (isa<Instruction>(Val) && API::isHeapAllocate((Instruction *)Val)) {
-//             Color = "red";
-//         }
-//         else if (isa<Argument>(Val) && API::isRustSinkForC((Argument *)Val)) {
-//             Color = "blue";
-//         }
-//         std::string Label = LabelGenerator(Val);
-//         VFGDot << "a" << NodeIdx << "[color=\"" << Color << "\""
-//                << "label=\"" << Label << "\"];\n";
-//         VFGNode2Idx.insert({VFGNode, NodeIdx});
-//         NodeIdx++;
-//     }
-//     for (auto VFGNode : AllInOne) {
-//         for (auto OutNode = VFGNode->begin(); OutNode != VFGNode->end(); OutNode++) {
-//             if (!VFGNode2Idx.count(OutNode->first)) {
-//                 continue;
-//             }
-//             VFGDot << "a" << VFGNode2Idx[VFGNode] << "->"
-//                    << "a" << VFGNode2Idx[OutNode->first];
-//             VFGDot << "[label=\"" << OutNode->second << "\"]"
-//                    << "\n";
-//         }
-//     }
-//     VFGDot << "}";
-//     VFGDot.close();
-// }
+            LabselBuilder << *Inst;
+        }
+        else if (auto Arg = dyn_cast<Argument>(Val)) {
+            LabselBuilder << Arg->getParent()->getName() << "\n" << *Arg;
+        }
+        auto LabelIdx = Label.find('\"');
+        while (LabelIdx != std::string::npos) {
+            Label.insert(LabelIdx, "\\");
+            LabelIdx = Label.find('\"', LabelIdx + 2);
+        }
+        return Label;
+    };
+
+    std::map<DyckVFGNode *, int> VFGNode2Idx;
+
+    int NodeIdx = 0;
+    std::error_code EC;
+    raw_fd_stream VFGDot(Filename.c_str(), EC);
+    VFGDot << "digraph rel{shape=box\n";
+    for (auto VFGNode : AllInOne) {
+        Value *Val = VFGNode->getValue();
+        std::string Color("black");
+        if (isa<Instruction>(Val) && API::isHeapAllocate((Instruction *)Val)) {
+            Color = "red";
+        }
+        else if (isa<Argument>(Val) && API::isRustSinkForC((Argument *)Val)) {
+            Color = "blue";
+        }
+        std::string Label = LabelGenerator(Val);
+        VFGDot << "a" << NodeIdx << "[color=\"" << Color << "\""
+               << "label=\"" << Label << "\"];\n";
+        VFGNode2Idx.insert({VFGNode, NodeIdx});
+        NodeIdx++;
+    }
+    for (auto VFGNode : AllInOne) {
+        for (auto OutNode = VFGNode->begin(); OutNode != VFGNode->end(); OutNode++) {
+            if (!VFGNode2Idx.count(OutNode->first)) {
+                continue;
+            }
+            VFGDot << "a" << VFGNode2Idx[VFGNode] << "->"
+                   << "a" << VFGNode2Idx[OutNode->first];
+            VFGDot << "[label=\"" << OutNode->second << "\"]"
+                   << "\n";
+        }
+    }
+    VFGDot << "}";
+    VFGDot.close();
+}
